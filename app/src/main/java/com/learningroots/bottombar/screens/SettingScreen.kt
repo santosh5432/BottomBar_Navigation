47313751f7d48e8afab716112d34f17b350702b2
Santosh Yadav <santosh_yadav321@yahoo.com>
2024-11-16 21:48:41 +0530

    Implementated bottom bar navigation in jetpack compose

---
+++ app/src/main/java/com/learningroots/bottombar/screens/SettingScreen.kt
@@ -0,0 +1,28 @@
+package com.learningroots.bottombar.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+/**
+ * Author: Santosh Yadav
+ * Created on: 16-11-2024 12:43
+ */
+
+@Composable
+fun SettingScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Setting Screen",
+            style = MaterialTheme.typography.headlineLarge
+        )
+    }
+}
